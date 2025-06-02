80f4e32506026fcf4eeb67cdd58b4c0545ab762d
ImoImoImokenpi <164855780+ImoImoImokenpi@users.noreply.github.com>
2025-06-02 16:41:35 +0900

    Delete haisya_realjam_only directory

--- haisya_realjam_only/create_db.py
+++
@@ -1,7 +0,0 @@
-from haisya_app.app import create_app, db
-
-def create_tables(event, context):
-    app = create_app()
-    with app.app_context():
-        db.create_all()
-        return "Tables created successfully!"
